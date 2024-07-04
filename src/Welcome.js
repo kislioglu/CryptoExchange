cd6edccbd44d64041b47dd5e210198eee7fb2e04
kislioglu <gkslogl@gmail.com>
2024-07-04 19:32:18 +0300

    first page we see when open app

---
+++ src/Welcome.js
@@ -0,0 +1,57 @@
+import {View, StyleSheet, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
+
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
+
+export default function Welcome() {
+  const navigation = useNavigation();
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Home');
+    }, 1000);
+  }, []);
+  useEffect(() => {
+    sv.value = withRepeat(withTiming(1, {duration, easing}), -1);
+  }, []);
+
+  const duration = 2000;
+  const easing = Easing.bezier(0.25, -0.5, 0.25, 1);
+
+  const sv = useSharedValue(0);
+
+  React.useEffect(() => {
+    sv.value = withRepeat(withTiming(1, {duration, easing}), -1);
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{rotate: `${sv.value * 360}deg`}],
+  }));
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.box, animatedStyle]} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    backgroundColor: 'black',
+  },
+  box: {
+    height: 120,
+    width: 120,
+    backgroundColor: '#b58df1',
+    borderRadius: 20,
+  },
+});
