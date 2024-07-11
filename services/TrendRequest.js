73773829e46caf6798aafd46233c35951340ea1a
kislioglu <gkslogl@gmail.com>
2024-07-11 17:12:08 +0300

    fetch request reformatted as async

--- services/TrendRequest.js
+++ services/TrendRequest.js
@@ -2,23 +2,33 @@ import {View, Text} from 'react-native';
 import React, {useEffect, useState} from 'react';
 
 export default function TrendsRequest() {
-
   const [trends, setTrends] = useState([]);
 
   useEffect(() => {
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        'x-cg-demo-api-key': 'CG-UAibsDpoq8yY5yjDzrVK2ZxS',
-      },
-    };
+    const fetchTrends = async () => {
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          'x-cg-demo-api-key': 'CG-UAibsDpoq8yY5yjDzrVK2ZxS',
+        },
+      };
 
-    fetch('https://api.coingecko.com/api/v3/search/trending', options)
-      .then(response => response.json())
-      .then(response => setTrends(response))
-      .catch(err => console.error(err));
+      try {
+        const response = await fetch(
+          'https://api.coingecko.com/api/v3/search/trending',
+          options,
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        setTrends(data);
+      } catch (error) {
+        console.error('An error occurred while fetching:', error);
+      }
+    };
+    fetchTrends();
   }, []);
-  
   return trends;
 }
