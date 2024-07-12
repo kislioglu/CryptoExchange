9925cc9283aecc113003fb97d76e5d6c0a8046f1
kislioglu <gkslogl@gmail.com>
2024-07-12 18:20:18 +0300

    copyright component created

---
+++ src/HomeScreen/Footer/Copyright.js
@@ -0,0 +1,72 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import {Image} from 'react-native';
+import {StyleSheet} from 'react-native';
+
+export default function Copyright() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.copyrightText}>
+        Copyright @ 2021 Ul8 LLC. All rights reserved
+      </Text>
+
+      <View style={styles.socialsView}>
+        <TouchableOpacity style={styles.eachSocialBtn}>
+          <Image
+            style={styles.eachImg}
+            source={require('../../../assets/socials/facebook.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.eachSocialBtn}>
+          <Image
+            style={styles.eachImg}
+            source={require('../../../assets/socials/twitter.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.eachSocialBtn}>
+          <Image
+            style={styles.eachImg}
+            source={require('../../../assets/socials/instagram.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.eachSocialBtn}>
+          <Image
+            style={styles.eachImg}
+            source={require('../../../assets/socials/dribble.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.eachSocialBtn}>
+          <Image
+            style={styles.eachImg}
+            source={require('../../../assets/socials/behance.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    top: 50,
+    height: 200,
+  },
+  copyrightText: {
+    alignSelf: 'center',
+    fontWeight: '500',
+  },
+  socialsView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 30,
+    gap: 20,
+  },
+  eachSocialBtn: {
+    width: 40,
+    height: 40,
+  },
+  eachImg: {
+    tintColor: '#777e90',
+  },
+});
