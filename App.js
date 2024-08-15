338a5ee7f8663ca2e2b4966414dd4d640f8fae6b
kislioglu <gkslogl@gmail.com>
2024-08-15 21:09:23 +0300

    unnecessary imports are removed

--- App.js
+++ App.js
@@ -5,19 +5,9 @@
  * @format
  */
 
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import Welcome from './src/Welcome';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from './src/HomeScreen/home/Home';
 import Navigation from './src/Navigation/Navigation';
-const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <Navigation />
-  );
+  return <Navigation />;
 }
-
-
