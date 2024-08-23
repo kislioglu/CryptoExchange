eee38d1737dfaa39a46748f25dfe190786c0db69
kislioglu <gkslogl@gmail.com>
2024-08-23 19:05:29 +0300

    ChartTrades component called

--- src/CoinCurrencyGraphic/Graphics/Chart/Chart.js
+++ src/CoinCurrencyGraphic/Graphics/Chart/Chart.js
@@ -1,11 +1,13 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import CoinCurrencyGraphics from '../../CoinCurrencyGraphics';
+import ChartTrades from './Trades/ChartTrades';
 
 export default function Chart({isValidData, spark}) {
   return (
     <View>
       <CoinCurrencyGraphics isValidData={isValidData} spark={spark} />
+      <ChartTrades />
     </View>
   );
 }
