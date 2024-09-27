4501a2510c07a0ca4f1a45c890aabb74ff4d8646
kislioglu <gkslogl@gmail.com>
2024-09-27 17:05:51 +0300

    hamburger menu added topright of the screen

--- src/HomeScreen/home/Home.js
+++ src/HomeScreen/home/Home.js
@@ -5,8 +5,9 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
+  Animated,
 } from 'react-native';
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Trends from '../marketTrends/Trends';
 import ContactUs from '../ContactUs/ContactUs';
 import {useNavigation} from '@react-navigation/native';
@@ -14,65 +15,149 @@ import DownloadApp from '../DownloadApp/DownloadApp';
 import JoinUs from '../JoinUs/JoinUs';
 import HowItWorks from '../HowItWorks/HowItWorks';
 import FooterNav from '../Footer/FooterNav';
+import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 export default function Home() {
   const navigation = useNavigation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const topLine = useRef(new Animated.Value(0)).current;
+  const bottomLine = useRef(new Animated.Value(0)).current;
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+
+    Animated.parallel([
+      Animated.timing(topLine, {
+        toValue: isOpen ? 0 : 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.timing(bottomLine, {
+        toValue: isOpen ? 0 : 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
 
   return (
-    <ScrollView
-      nestedScrollEnabled={true}
-      style={{backgroundColor: '#fff', height: '100%'}}>
-      <View style={styles.logoView}>
-        <Image
-          style={styles.logoImg}
-          source={require('../../../assets/logo.png')}
-        />
-        <Text style={styles.companyNameText}>Coiner</Text>
-      </View>
-      <View style={styles.sloganView}>
-        <View style={styles.captionView}>
-          <Text style={styles.captionText}>Buy & sell crypto in minutes</Text>
+    <View>
+      <View
+        style={{
+          width: '100%',
+          height: 80,
+          zIndex: 10,
+          justifyContent: 'center',
+          backgroundColor: '#fff',
+        }}>
+        <View style={styles.logoView}>
+          <View style={{flexDirection: 'row', gap: 5, marginLeft: 20}}>
+            <Image
+              style={styles.logoImg}
+              source={require('../../../assets/logo.png')}
+            />
+            <Text style={styles.companyNameText}>Coiner</Text>
+          </View>
+          <TouchableOpacity onPress={toggleMenu}>
+            <View style={styles.container}>
+              <Animated.View
+                style={[
+                  styles.line,
+                  {
+                    transform: [
+                      {
+                        translateY: topLine.interpolate({
+                          inputRange: [0, 1],
+                          outputRange: [0, 4],
+                        }),
+                      },
+                      {
+                        rotate: topLine.interpolate({
+                          inputRange: [0, 1],
+                          outputRange: ['0deg', '45deg'],
+                        }),
+                      },
+                    ],
+                  },
+                ]}
+              />
+              <Animated.View
+                style={[
+                  styles.line,
+                  {
+                    transform: [
+                      {
+                        translateY: bottomLine.interpolate({
+                          inputRange: [0, 1],
+                          outputRange: [0, -2],
+                        }),
+                      },
+                      {
+                        rotate: bottomLine.interpolate({
+                          inputRange: [0, 1],
+                          outputRange: ['0deg', '-45deg'],
+                        }),
+                      },
+                    ],
+                  },
+                ]}
+              />
+            </View>
+          </TouchableOpacity>
         </View>
-        <View style={styles.explanationView}>
-          <Text style={styles.explanationText}>
-            Trade Bitcoin, Etherium, USDT and the top altcoins on the legendary
-            crypto asset exchange.
-          </Text>
+      </View>
+      {isOpen ? (
+        <HamburgerMenu />
+      ) : null}
+      <ScrollView
+        nestedScrollEnabled={true}
+        style={{backgroundColor: '#fff', height: '100%'}}>
+        <View style={styles.sloganView}>
+          <View style={styles.captionView}>
+            <Text style={styles.captionText}>Buy & sell crypto in minutes</Text>
+          </View>
+          <View style={styles.explanationView}>
+            <Text style={styles.explanationText}>
+              Trade Bitcoin, Etherium, USDT and the top altcoins on the
+              legendary crypto asset exchange.
+            </Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.navigateButtonView}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('Signup')}
-          style={styles.navigateButton}
-          activeOpacity={0.3}>
-          <Text style={styles.navigateButtonText}>Get started now</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.walletImgView}>
-        <Image
-          style={styles.walletImg}
-          source={require('../../../assets/walletImg.png')}
-        />
-      </View>
+        <View style={styles.navigateButtonView}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Signup')}
+            style={styles.navigateButton}
+            activeOpacity={0.3}>
+            <Text style={styles.navigateButtonText}>Get started now</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.walletImgView}>
+          <Image
+            style={styles.walletImg}
+            source={require('../../../assets/walletImg.png')}
+          />
+        </View>
 
-      <View style={{backgroundColor: '#fff'}}>
-        <Trends />
-      </View>
-      <View style={{backgroundColor: '#f4f5f6'}}>
-        <ContactUs />
-      </View>
-      <View style={{backgroundColor: '#fff'}}>
-        <DownloadApp />
-      </View>
-      <View style={{backgroundColor: '#f4f5f6'}}>
-        <JoinUs />
-      </View>
-      <View style={{backgroundColor: '#fff'}}>
-        <HowItWorks />
-      </View>
-      <View style={{backgroundColor: '#fff'}}>
-        <FooterNav />
-      </View>
-    </ScrollView>
+        <View style={{backgroundColor: '#fff'}}>
+          <Trends />
+        </View>
+        <View style={{backgroundColor: '#f4f5f6'}}>
+          <ContactUs />
+        </View>
+        <View style={{backgroundColor: '#fff'}}>
+          <DownloadApp />
+        </View>
+        <View style={{backgroundColor: '#f4f5f6'}}>
+          <JoinUs />
+        </View>
+        <View style={{backgroundColor: '#fff'}}>
+          <HowItWorks />
+        </View>
+        <View style={{backgroundColor: '#fff'}}>
+          <FooterNav />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
@@ -80,9 +165,8 @@ const styles = StyleSheet.create({
   logoView: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 5,
-    marginTop: 40,
-    marginLeft: 20,
+    height: '100%',
+    justifyContent: 'space-between',
   },
   logoImg: {
     width: 32,
@@ -145,5 +229,18 @@ const styles = StyleSheet.create({
   walletImg: {
     width: '100%',
     height: '100%',
+  },
+  container: {
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20,
+  },
+  line: {
+    width: 20,
+    height: 2,
+    backgroundColor: '#777e90',
+    marginVertical: 2,
   },
 });
