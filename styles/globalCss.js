898cef20a5fb9836682ee5ac83dc4722c493383f
kislioglu <gkslogl@gmail.com>
2024-10-08 17:43:07 +0300

    global css created for recurring css properties

---
+++ styles/globalCss.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const globalCss = StyleSheet.create({
+  textColorGrey: {
+    color: '#777e90',
+  },
+  btnPrimary: {
+    backgroundColor: '#3772ff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
