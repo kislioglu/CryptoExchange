8424f8403d6cafd3db7fd9867bb37478da807959
kislioglu <gkslogl@gmail.com>
2024-10-08 17:45:19 +0300

    hamburger menu codes moved from here

--- src/HomeScreen/home/Home.js
+++ src/HomeScreen/home/Home.js
@@ -5,9 +5,8 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
-  Animated,
 } from 'react-native';
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import Trends from '../marketTrends/Trends';
 import ContactUs from '../ContactUs/ContactUs';
 import {useNavigation} from '@react-navigation/native';
@@ -15,100 +14,16 @@ import DownloadApp from '../DownloadApp/DownloadApp';
 import JoinUs from '../JoinUs/JoinUs';
 import HowItWorks from '../HowItWorks/HowItWorks';
 import FooterNav from '../Footer/FooterNav';
-import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
+import {globalCss} from '../../../styles/globalCss';
+import Header from '../Header/Header';
 export default function Home() {
   const navigation = useNavigation();
-  const [isOpen, setIsOpen] = useState(false);
-
-  const topLine = useRef(new Animated.Value(0)).current;
-  const bottomLine = useRef(new Animated.Value(0)).current;
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-
-    Animated.parallel([
-      Animated.timing(topLine, {
-        toValue: isOpen ? 0 : 1,
-        duration: 300,
-        useNativeDriver: true,
-      }),
-      Animated.timing(bottomLine, {
-        toValue: isOpen ? 0 : 1,
-        duration: 300,
-        useNativeDriver: true,
-      }),
-    ]).start();
-  };
 
   return (
     <View>
-      <View
-        style={{
-          width: '100%',
-          height: 80,
-          zIndex: 10,
-          justifyContent: 'center',
-          backgroundColor: '#fff',
-        }}>
-        <View style={styles.logoView}>
-          <View style={{flexDirection: 'row', gap: 5, marginLeft: 20}}>
-            <Image
-              style={styles.logoImg}
-              source={require('../../../assets/logo.png')}
-            />
-            <Text style={styles.companyNameText}>Bitcloud</Text>
-          </View>
-          <TouchableOpacity onPress={toggleMenu}>
-            <View style={styles.container}>
-              <Animated.View
-                style={[
-                  styles.line,
-                  {
-                    transform: [
-                      {
-                        translateY: topLine.interpolate({
-                          inputRange: [0, 1],
-                          outputRange: [0, 4],
-                        }),
-                      },
-                      {
-                        rotate: topLine.interpolate({
-                          inputRange: [0, 1],
-                          outputRange: ['0deg', '45deg'],
-                        }),
-                      },
-                    ],
-                  },
-                ]}
-              />
-              <Animated.View
-                style={[
-                  styles.line,
-                  {
-                    transform: [
-                      {
-                        translateY: bottomLine.interpolate({
-                          inputRange: [0, 1],
-                          outputRange: [0, -2],
-                        }),
-                      },
-                      {
-                        rotate: bottomLine.interpolate({
-                          inputRange: [0, 1],
-                          outputRange: ['0deg', '-45deg'],
-                        }),
-                      },
-                    ],
-                  },
-                ]}
-              />
-            </View>
-          </TouchableOpacity>
-        </View>
+      <View>
+        <Header />
       </View>
-      {isOpen ? (
-        <HamburgerMenu />
-      ) : null}
       <ScrollView
         nestedScrollEnabled={true}
         style={{backgroundColor: '#fff', height: '100%'}}>
@@ -117,13 +32,13 @@ export default function Home() {
             <Text style={styles.captionText}>Buy & sell crypto in minutes</Text>
           </View>
           <View style={styles.explanationView}>
-            <Text style={styles.explanationText}>
+            <Text style={[styles.explanationText, globalCss.textColorGrey]}>
               Trade Bitcoin, Etherium, USDT and the top altcoins on the
               legendary crypto asset exchange.
             </Text>
           </View>
         </View>
-        <View style={styles.navigateButtonView}>
+        <View style={[styles.navigateButtonView, globalCss.btnPrimary]}>
           <TouchableOpacity
             onPress={() => navigation.navigate('Signup')}
             style={styles.navigateButton}
@@ -162,21 +77,6 @@ export default function Home() {
 }
 
 const styles = StyleSheet.create({
-  logoView: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    height: '100%',
-    justifyContent: 'space-between',
-  },
-  logoImg: {
-    width: 32,
-    height: 32,
-  },
-  companyNameText: {
-    fontWeight: 'bold',
-    fontSize: 24,
-    color: '#000',
-  },
   sloganView: {
     gap: 20,
     width: '100%',
@@ -197,7 +97,7 @@ const styles = StyleSheet.create({
   explanationText: {
     fontWeight: '500',
     fontSize: 16,
-    color: '#777e90',
+
     letterSpacing: 0.2,
   },
   navigateButtonView: {
@@ -206,8 +106,6 @@ const styles = StyleSheet.create({
     borderRadius: 40,
     marginLeft: 20,
     marginTop: 30,
-    justifyContent: 'center',
-    backgroundColor: '#3772ff',
   },
   navigateButton: {
     width: '100%',
