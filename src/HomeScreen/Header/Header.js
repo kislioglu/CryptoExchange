1892abb0d60467384857c6df70b79b52246d2ba1
kislioglu <gkslogl@gmail.com>
2024-10-08 17:48:46 +0300

    header component created for contain header components

---
+++ src/HomeScreen/Header/Header.js
@@ -0,0 +1,13 @@
+import {View, StyleSheet} from 'react-native';
+import React from 'react';
+import HamburgerMenuToggle from '../HamburgerMenu/HamburgerMenuToggle';
+
+export default function Header() {
+  return (
+    <View>
+      <HamburgerMenuToggle />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
