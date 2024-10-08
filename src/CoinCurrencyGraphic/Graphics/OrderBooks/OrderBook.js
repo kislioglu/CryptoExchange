e6b49cd0148e5fc1e0131576b3bfc760d785c093
kislioglu <gkslogl@gmail.com>
2024-10-08 17:49:57 +0300

    component created but not coded yet

---
+++ src/CoinCurrencyGraphic/Graphics/OrderBooks/OrderBook.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function OrderBook() {
+  return (
+    <View>
+      <Text>OrderBook</Text>
+    </View>
+  );
+}
