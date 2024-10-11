6301a9a20748df6f5eee0929568e61d255481915
kislioglu <gkslogl@gmail.com>
2024-10-11 16:32:26 +0300

    added global white text color

--- styles/globalCss.js
+++ styles/globalCss.js
@@ -4,6 +4,9 @@ export const globalCss = StyleSheet.create({
   textColorGrey: {
     color: '#777e90',
   },
+  textColorWhite: {
+    color: '#fff',
+  },
   btnPrimary: {
     backgroundColor: '#3772ff',
     justifyContent: 'center',
