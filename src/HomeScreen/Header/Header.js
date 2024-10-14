4a418fdfa2025f064a032042cd200ece8a1cb5e2
kislioglu <gkslogl@gmail.com>
2024-10-14 17:29:54 +0300

    company logo and hamburger menu call moved here

--- src/HomeScreen/Header/Header.js
+++ src/HomeScreen/Header/Header.js
@@ -1,13 +1,60 @@
-import {View, StyleSheet} from 'react-native';
-import React from 'react';
+import {View, StyleSheet, Image, Animated} from 'react-native';
+import React, {useRef} from 'react';
 import HamburgerMenuToggle from '../HamburgerMenu/HamburgerMenuToggle';
+import useMenuStore from '../HamburgerMenu/zustand/UseMenuStore';
+import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 
 export default function Header() {
+  const {isOpen} = useMenuStore();
+
+  const slideAnim = useRef(new Animated.Value(-30000)).current;
+
+  const slideAnimation = Animated.timing(slideAnim, {
+    toValue: isOpen ? -30000 : 0,
+    duration: 30000,
+    useNativeDriver: true,
+  }).start();
+
   return (
-    <View>
-      <HamburgerMenuToggle />
+    <View
+      style={{
+        minHeight: 80,
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <View
+        style={{
+          position: 'absolute',
+          top: 20,
+          left: 0,
+          marginLeft: 20,
+          zIndex: 10000,
+        }}>
+        <Image
+          style={styles.logoImg}
+          source={require('../../../assets/logo.png')}
+        />
+      </View>
+      <View
+        style={{
+          height: '100%',
+          top: 20,
+          right: 20,
+          width: 40,
+          position: 'absolute',
+        }}>
+        <HamburgerMenuToggle />
+      </View>
+      <View style={[slideAnimation]}>{isOpen ? <HamburgerMenu /> : null}</View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  logoImg: {
+    width: 32,
+    height: 32,
+  },
+});
