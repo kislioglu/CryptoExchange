dfdc2559c51e4fb4c1c3ad0f2a791fd2123d9116
kislioglu <gkslogl@gmail.com>
2024-10-15 17:56:26 +0300

    transparent background changed as non-transparent

--- src/HomeScreen/Header/LanguageAndCurrencySelector/LanguageAndCurrencySelector.js
+++ src/HomeScreen/Header/LanguageAndCurrencySelector/LanguageAndCurrencySelector.js
@@ -55,126 +55,135 @@ export default function LanguageAndCurrencySelector() {
         onRequestClose={() => setIsSelectorVisible(false)}>
         <View
           style={{
-            backgroundColor: '#fff',
-            width: '90%',
-            height: 280,
-            top: 100,
-            marginHorizontal: 20,
-            borderRadius: 20,
-            shadowColor: '#000',
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 4,
-            elevation: 5,
+            flex: 1,
+            backgroundColor: 'rgba(0,0,0,0.2)',
           }}>
-          <TouchableOpacity
-            style={{
-              backgroundColor: '#fff',
-              position: 'absolute',
-              right: 10,
-              top: 10,
-            }}
-            onPress={() => setIsSelectorVisible(false)}>
-            <Image
-              style={styles.downArrowImg}
-              source={require('../../../../assets/down.png')}
-            />
-          </TouchableOpacity>
           <View
             style={{
-              flexDirection: 'row',
-              top: 30,
-              justifyContent: 'space-evenly',
+              backgroundColor: '#fff',
+              width: '90%',
+              height: 280,
+              top: 100,
+              marginHorizontal: 20,
+              borderRadius: 20,
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 4,
+              elevation: 5,
             }}>
-            <View style={{gap: 10, width: '40%'}}>
-              <Text
-                style={[
-                  globalCss.textColorGrey,
-                  {fontSize: 12, fontWeight: '600'},
-                ]}>
-                Language
-              </Text>
-              {languages.map((ln, index) => (
-                <TouchableOpacity
-                  onPress={() => setSelectedLanguage(ln.symbol)}
-                  style={{
-                    flexDirection: 'row',
-                    alignItems: 'flex-end',
-                    gap: 10,
-                    borderBottomWidth: index !== languages.length - 1 ? 1 : 0,
-                    paddingBottom: 10,
-                    borderColor: '#e6e8ec',
-                  }}
-                  key={index}>
-                  <Text
-                    style={[
-                      globalCss.textColorGrey,
-                      {fontSize: 10, fontWeight: 'bold'},
-                      selectedLanguage === ln.symbol ? {color: '#000'} : null,
-                    ]}>
-                    {ln.symbol}
-                  </Text>
-                  <Text
-                    style={[
-                      globalCss.textColorGrey,
-                      {fontWeight: 'bold'},
-                      selectedLanguage === ln.symbol ? {color: '#000'} : null,
-                    ]}>
-                    {ln.language}
-                  </Text>
-                </TouchableOpacity>
-              ))}
-            </View>
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#fff',
+                position: 'absolute',
+                right: 10,
+                top: 10,
+              }}
+              onPress={() => setIsSelectorVisible(false)}>
+              <Image
+                style={styles.downArrowImg}
+                source={require('../../../../assets/down.png')}
+              />
+            </TouchableOpacity>
             <View
-              style={{width: 2, height: '100%', backgroundColor: '#e6e8ec'}}
-            />
-            <View style={{gap: 10, width: '40%'}}>
-              <Text
-                style={[
-                  globalCss.textColorGrey,
-                  {fontSize: 12, fontWeight: '600'},
-                ]}>
-                Currency
-              </Text>
-              {currencies.map((cr, index) => (
-                <TouchableOpacity
-                  onPress={() => setSelectedCurrency(cr.symbol)}
-                  style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    gap: 10,
-                    borderBottomWidth: index !== currencies.length - 1 ? 1 : 0,
-                    paddingBottom: 10,
-                    borderColor: '#e6e8ec',
-                    paddingLeft: 10,
-                  }}
-                  key={index}>
-                  <View
-                    style={[
-                      {
-                        width: 10,
-                        height: 10,
-                        borderRadius: 10,
-                        backgroundColor: '#e6e8ec',
-                      },
-                      selectedCurrency === cr.currency
-                        ? {backgroundColor: '#000'}
-                        : null,
-                    ]}
-                  />
-                  <Text
-                    style={[
-                      globalCss.textColorGrey,
-                      {fontWeight: 'bold'},
-                      selectedCurrency === cr.currency ? {color: '#000'} : null,
-                    ]}>
-                    {cr.currency}
-                  </Text>
-                </TouchableOpacity>
-              ))}
+              style={{
+                flexDirection: 'row',
+                top: 30,
+                justifyContent: 'space-evenly',
+              }}>
+              <View style={{gap: 10, width: '40%'}}>
+                <Text
+                  style={[
+                    globalCss.textColorGrey,
+                    {fontSize: 12, fontWeight: '600'},
+                  ]}>
+                  Language
+                </Text>
+                {languages.map((ln, index) => (
+                  <TouchableOpacity
+                    onPress={() => setSelectedLanguage(ln.symbol)}
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'flex-end',
+                      gap: 10,
+                      borderBottomWidth: index !== languages.length - 1 ? 1 : 0,
+                      paddingBottom: 10,
+                      borderColor: '#e6e8ec',
+                    }}
+                    key={index}>
+                    <Text
+                      style={[
+                        globalCss.textColorGrey,
+                        {fontSize: 10, fontWeight: 'bold'},
+                        selectedLanguage === ln.symbol ? {color: '#000'} : null,
+                      ]}>
+                      {ln.symbol}
+                    </Text>
+                    <Text
+                      style={[
+                        globalCss.textColorGrey,
+                        {fontWeight: 'bold'},
+                        selectedLanguage === ln.symbol ? {color: '#000'} : null,
+                      ]}>
+                      {ln.language}
+                    </Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+              <View
+                style={{width: 2, height: '100%', backgroundColor: '#e6e8ec'}}
+              />
+              <View style={{gap: 10, width: '40%'}}>
+                <Text
+                  style={[
+                    globalCss.textColorGrey,
+                    {fontSize: 12, fontWeight: '600'},
+                  ]}>
+                  Currency
+                </Text>
+                {currencies.map((cr, index) => (
+                  <TouchableOpacity
+                    onPress={() => setSelectedCurrency(cr.symbol)}
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      gap: 10,
+                      borderBottomWidth:
+                        index !== currencies.length - 1 ? 1 : 0,
+                      paddingBottom: 10,
+                      borderColor: '#e6e8ec',
+                      paddingLeft: 10,
+                    }}
+                    key={index}>
+                    <View
+                      style={[
+                        {
+                          width: 10,
+                          height: 10,
+                          borderRadius: 10,
+                          backgroundColor: '#e6e8ec',
+                        },
+                        selectedCurrency === cr.currency
+                          ? {backgroundColor: '#000'}
+                          : null,
+                      ]}
+                    />
+                    <Text
+                      style={[
+                        globalCss.textColorGrey,
+                        {fontWeight: 'bold'},
+                        selectedCurrency === cr.currency
+                          ? {color: '#000'}
+                          : null,
+                      ]}>
+                      {cr.currency}
+                    </Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
             </View>
           </View>
         </View>
