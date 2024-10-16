bcfd8852cb7eb535d9e6a2d40c7a4c9fc21cd9e8
kislioglu <gkslogl@gmail.com>
2024-10-16 18:02:18 +0300

    states are now coming from context and added new currencies

--- src/HomeScreen/Header/LanguageAndCurrencySelector/LanguageAndCurrencySelector.js
+++ src/HomeScreen/Header/LanguageAndCurrencySelector/LanguageAndCurrencySelector.js
@@ -4,27 +4,88 @@ import {
   TouchableOpacity,
   Modal,
   Image,
-  Dimensions,
   StyleSheet,
+  ScrollView,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {globalCss} from '../../../../styles/globalCss';
+import {LanguageAndCurrencyContext} from './LanguageAndCurrencyContext';
 
 export default function LanguageAndCurrencySelector() {
-  const [selectedLanguage, setSelectedLanguage] = useState('US');
-  const [selectedCurrency, setSelectedCurrency] = useState('USD');
+  const {
+    selectedLanguage,
+    setSelectedLanguage,
+    selectedCurrency,
+    setSelectedCurrency,
+  } = useContext(LanguageAndCurrencyContext);
   const [isSelectorVisible, setIsSelectorVisible] = useState(false);
   const languages = [
     {symbol: 'US', language: 'English'},
     {symbol: 'VN', language: 'Viatnamese'},
-    {symbol: 'Ru', language: 'Russia'},
+    {symbol: 'RU', language: 'Russia'},
   ];
   const currencies = [
-    {symbol: 'USD', currency: 'USD'},
-    {symbol: 'RUB', currency: 'RUB'},
-    {symbol: 'EUR', currency: 'EUR'},
-    {symbol: 'JPY', currency: 'JPY'},
-    {symbol: 'BTC', currency: 'BTC'},
+    {symbol: 'btc'},
+    {symbol: 'eth'},
+    {symbol: 'ltc'},
+    {symbol: 'bch'},
+    {symbol: 'eos'},
+    {symbol: 'xrp'},
+    {symbol: 'xlm'},
+    {symbol: 'link'},
+    {symbol: 'dot'},
+    {symbol: 'yfi'},
+    {symbol: 'usd'},
+    {symbol: 'aed'},
+    {symbol: 'ars'},
+    {symbol: 'aud'},
+    {symbol: 'bdt'},
+    {symbol: 'bhd'},
+    {symbol: 'bmd'},
+    {symbol: 'brl'},
+    {symbol: 'cad'},
+    {symbol: 'chf'},
+    {symbol: 'clp'},
+    {symbol: 'cny'},
+    {symbol: 'czk'},
+    {symbol: 'dkk'},
+    {symbol: 'eur'},
+    {symbol: 'gbp'},
+    {symbol: 'gel'},
+    {symbol: 'hkd'},
+    {symbol: 'huf'},
+    {symbol: 'idr'},
+    {symbol: 'ils'},
+    {symbol: 'inr'},
+    {symbol: 'jpy'},
+    {symbol: 'krw'},
+    {symbol: 'kwd'},
+    {symbol: 'lkr'},
+    {symbol: 'mmk'},
+    {symbol: 'mxn'},
+    {symbol: 'myr'},
+    {symbol: 'ngn'},
+    {symbol: 'nok'},
+    {symbol: 'nzd'},
+    {symbol: 'php'},
+    {symbol: 'pkr'},
+    {symbol: 'pln'},
+    {symbol: 'rub'},
+    {symbol: 'sar'},
+    {symbol: 'sek'},
+    {symbol: 'sgd'},
+    {symbol: 'thb'},
+    {symbol: 'try'},
+    {symbol: 'twd'},
+    {symbol: 'uah'},
+    {symbol: 'vef'},
+    {symbol: 'vnd'},
+    {symbol: 'zar'},
+    {symbol: 'xdr'},
+    {symbol: 'xag'},
+    {symbol: 'xau'},
+    {symbol: 'bits'},
+    {symbol: 'sats'},
   ];
   return (
     <View>
@@ -41,7 +102,9 @@ export default function LanguageAndCurrencySelector() {
         }}>
         <Text style={styles.currencyAndLanguage}>{selectedLanguage}</Text>
         <Text style={styles.currencyAndLanguage}>{'/'}</Text>
-        <Text style={styles.currencyAndLanguage}>{selectedCurrency}</Text>
+        <Text style={styles.currencyAndLanguage}>
+          {selectedCurrency.toUpperCase()}
+        </Text>
         <Image
           style={styles.downArrowImg}
           source={require('../../../../assets/down.png')}
@@ -144,45 +207,47 @@ export default function LanguageAndCurrencySelector() {
                   ]}>
                   Currency
                 </Text>
-                {currencies.map((cr, index) => (
-                  <TouchableOpacity
-                    onPress={() => setSelectedCurrency(cr.symbol)}
-                    style={{
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      gap: 10,
-                      borderBottomWidth:
-                        index !== currencies.length - 1 ? 1 : 0,
-                      paddingBottom: 10,
-                      borderColor: '#e6e8ec',
-                      paddingLeft: 10,
-                    }}
-                    key={index}>
-                    <View
-                      style={[
-                        {
-                          width: 10,
-                          height: 10,
-                          borderRadius: 10,
-                          backgroundColor: '#e6e8ec',
-                        },
-                        selectedCurrency === cr.currency
-                          ? {backgroundColor: '#000'}
-                          : null,
-                      ]}
-                    />
-                    <Text
-                      style={[
-                        globalCss.textColorGrey,
-                        {fontWeight: 'bold'},
-                        selectedCurrency === cr.currency
-                          ? {color: '#000'}
-                          : null,
-                      ]}>
-                      {cr.currency}
-                    </Text>
-                  </TouchableOpacity>
-                ))}
+                <ScrollView style={{height: 190}}>
+                  {currencies.map((cr, index) => (
+                    <TouchableOpacity
+                      onPress={() => setSelectedCurrency(cr.symbol)}
+                      style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        gap: 10,
+                        borderBottomWidth:
+                          index !== currencies.length - 1 ? 1 : 0,
+                        paddingVertical: 10,
+                        borderColor: '#e6e8ec',
+                        paddingLeft: 10,
+                      }}
+                      key={index}>
+                      <View
+                        style={[
+                          {
+                            width: 10,
+                            height: 10,
+                            borderRadius: 10,
+                            backgroundColor: '#e6e8ec',
+                          },
+                          selectedCurrency === cr.symbol
+                            ? {backgroundColor: '#000'}
+                            : null,
+                        ]}
+                      />
+                      <Text
+                        style={[
+                          globalCss.textColorGrey,
+                          {fontWeight: 'bold'},
+                          selectedCurrency === cr.symbol
+                            ? {color: '#000'}
+                            : null,
+                        ]}>
+                        {cr.symbol}
+                      </Text>
+                    </TouchableOpacity>
+                  ))}
+                </ScrollView>
               </View>
             </View>
           </View>
