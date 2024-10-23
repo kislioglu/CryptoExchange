7a7e3ace1213ed1025b56e7e1548b2850d0f7bf4
kislioglu <gkslogl@gmail.com>
2024-10-23 19:20:26 +0300

    static notification instances created

---
+++ staticDatas/Notifications.js
@@ -0,0 +1,44 @@
+export const staticNotifications = [
+  {
+    id: 1,
+    date: '2021-03-10',
+    hours: '20:19:15',
+    notification: 'Login attempted from new IP',
+    isSeen: false,
+  },
+  {
+    id: 2,
+    date: '2021-03-10',
+    hours: '20:19:15',
+    notification: 'Request to reset security',
+    isSeen: false,
+  },
+  {
+    id: 3,
+    date: '2021-03-10',
+    hours: '20:19:15',
+    notification: 'Login attempted from new IP',
+    isSeen: false,
+  },
+  {
+    id: 4,
+    date: '2021-03-10',
+    hours: '20:19:15',
+    notification: 'Request to reset security',
+    isSeen: false,
+  },
+  {
+    id: 5,
+    date: '2021-03-10',
+    hours: '20:19:15',
+    notification: 'Login attempted from new IP',
+    isSeen: true,
+  },
+  {
+    id: 6,
+    date: '2021-03-10',
+    hours: '20:19:15',
+    notification: 'Login attempted from new IP',
+    isSeen: true,
+  },
+];
