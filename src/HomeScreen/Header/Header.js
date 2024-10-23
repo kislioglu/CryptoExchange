673f98157f6585cdd53842491fb6241f5f2e6fac
kislioglu <gkslogl@gmail.com>
2024-10-23 19:15:02 +0300

    userProfile and Notifications are added

--- src/HomeScreen/Header/Header.js
+++ src/HomeScreen/Header/Header.js
@@ -1,12 +1,22 @@
-import {View, StyleSheet, Image, Animated} from 'react-native';
+import {View, StyleSheet, Image, Animated, Dimensions} from 'react-native';
 import React, {useRef} from 'react';
 import HamburgerMenuToggle from '../HamburgerMenu/HamburgerMenuToggle';
-import useMenuStore from '../HamburgerMenu/zustand/UseMenuStore';
+import {
+  useMenuStore,
+  useNotificationStore,
+  useProfileStore,
+} from '../HamburgerMenu/zustand/UseMenuStore';
 import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 import LanguageAndCurrencySelector from './LanguageAndCurrencySelector/LanguageAndCurrencySelector';
+import UserProfile from './UserProfile/UserProfile';
+import UserProfileButton from './UserProfile/UserProfileButton';
+import Notifications from './Notifications/Notifications';
+import NotificationsButtton from './Notifications/NotificationsButtton';
 
 export default function Header() {
   const {isOpen} = useMenuStore();
+  const {isOpened} = useProfileStore();
+  const {showNotifications} = useNotificationStore();
 
   const slideAnim = useRef(new Animated.Value(-30000)).current;
 
@@ -25,6 +35,7 @@ export default function Header() {
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#fff',
+        zIndex: 10000,
       }}>
       <View
         style={{
@@ -33,6 +44,7 @@ export default function Header() {
           left: 0,
           marginLeft: 20,
           zIndex: 10000,
+          marginTop: 5,
         }}>
         <Image
           style={styles.logoImg}
@@ -42,13 +54,37 @@ export default function Header() {
       <View
         style={{
           height: 60,
-          right: 100,
+          left: 60,
+          width: 100,
+          position: 'absolute',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          top: 10,
+        }}>
+        <LanguageAndCurrencySelector />
+      </View>
+      <View
+        style={{
+          height: 60,
+          width: 100,
+          position: 'absolute',
+          justifyContent: 'center',
+          alignItems: 'flex-end',
+          top: 10,
+        }}>
+        <NotificationsButtton />
+      </View>
+      <View
+        style={{
+          height: 60,
+          right: 60,
           width: 100,
           position: 'absolute',
           justifyContent: 'center',
           alignItems: 'center',
+          top: 10,
         }}>
-        <LanguageAndCurrencySelector />
+        <UserProfileButton />
       </View>
       <View
         style={{
@@ -60,7 +96,46 @@ export default function Header() {
         }}>
         <HamburgerMenuToggle />
       </View>
-      <View style={[slideAnimation]}>{isOpen ? <HamburgerMenu /> : null}</View>
+      <View style={[slideAnimation, {position: 'absolute', top: 0}]}>
+        {isOpen ? <HamburgerMenu /> : null}
+      </View>
+      <View
+        style={{
+          top: 80,
+          position: 'absolute',
+          width: '90%',
+          backgroundColor: '#fff',
+          borderRadius: 20,
+          paddingHorizontal: 20,
+          elevation: 10,
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 4,
+        }}>
+        {isOpened ? <UserProfile /> : null}
+      </View>
+      <View
+        style={{
+          top: 80,
+          width: '90%',
+          backgroundColor: '#fff',
+          borderRadius: 20,
+          paddingHorizontal: 20,
+          elevation: 10,
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 4,
+        }}>
+        {showNotifications ? <Notifications /> : null}
+      </View>
     </View>
   );
 }
