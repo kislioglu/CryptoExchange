4e5fccc06ee7bff421db7924bf63ba7b9a540c53
kislioglu <gkslogl@gmail.com>
2024-10-30 16:41:05 +0300

    icons path for static notification data

---
+++ staticDatas/notificationIconsPath.js
@@ -0,0 +1,9 @@
+export const icons = {
+  Wallet: require('../assets/notificationImages/wallet.png'),
+  Security: require('../assets/notificationImages/security.png'),
+  Trade: require('../assets/notificationImages/trade.png'),
+  Deposit: require('../assets/notificationImages/deposit.png'),
+  Transfer: require('../assets/notificationImages/transfer.png'),
+  Withdrawals: require('../assets/notificationImages/withdrawals.png'),
+  News: require('../assets/notificationImages/news.png'),
+};
