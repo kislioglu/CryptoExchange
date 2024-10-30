91609e016f1ccbf146d1efc3ae085190f34ec3a6
kislioglu <gkslogl@gmail.com>
2024-10-30 16:41:59 +0300

    2 more features added to each notification

--- staticDatas/Notifications.js
+++ staticDatas/Notifications.js
@@ -4,41 +4,69 @@ export const staticNotifications = [
     date: '2021-03-10',
     hours: '20:19:15',
     notification: 'Login attempted from new IP',
+    notificationLong:
+      'Hello, you have reset the Google Authentication on your account successfully. Your old security items have expired and new security items have now been enabled.',
     isSeen: false,
+    type: 'Security',
   },
   {
     id: 2,
     date: '2021-03-10',
     hours: '20:19:15',
     notification: 'Request to reset security',
+    notificationLong:
+      'Hello, you have reset the Google Authentication on your account successfully. Your old security items have expired and new security items have now been enabled.',
     isSeen: false,
+    type: 'Wallet',
   },
   {
     id: 3,
     date: '2021-03-10',
     hours: '20:19:15',
     notification: 'Login attempted from new IP',
+    notificationLong:
+      'Hello, you have reset the Google Authentication on your account successfully. Your old security items have expired and new security items have now been enabled.',
     isSeen: false,
+    type: 'Trade',
   },
   {
     id: 4,
     date: '2021-03-10',
     hours: '20:19:15',
     notification: 'Request to reset security',
+    notificationLong:
+      'Hello, you have reset the Google Authentication on your account successfully. Your old security items have expired and new security items have now been enabled.',
     isSeen: false,
+    type: 'Deposit',
   },
   {
     id: 5,
     date: '2021-03-10',
     hours: '20:19:15',
     notification: 'Login attempted from new IP',
+    notificationLong:
+      'Hello, you have reset the Google Authentication on your account successfully. Your old security items have expired and new security items have now been enabled.',
     isSeen: true,
+    type: 'Transfer',
   },
   {
     id: 6,
     date: '2021-03-10',
     hours: '20:19:15',
     notification: 'Login attempted from new IP',
+    notificationLong:
+      'Hello, you have reset the Google Authentication on your account successfully. Your old security items have expired and new security items have now been enabled.',
     isSeen: true,
+    type: 'Withdrawals',
+  },
+  {
+    id: 6,
+    date: '2021-03-10',
+    hours: '20:19:15',
+    notification: 'Login attempted from new IP',
+    notificationLong:
+      'Hello, you have reset the Google Authentication on your account successfully. Your old security items have expired and new security items have now been enabled.',
+    isSeen: true,
+    type: 'News',
   },
 ];
