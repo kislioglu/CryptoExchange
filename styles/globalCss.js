f7b61e92c88c8947b4c2b1c21386e9637ca26c55
kislioglu <gkslogl@gmail.com>
2024-10-30 16:42:14 +0300

    new text color added

--- styles/globalCss.js
+++ styles/globalCss.js
@@ -4,6 +4,9 @@ export const globalCss = StyleSheet.create({
   textColorGrey: {
     color: '#777e90',
   },
+  textColorKindaBlack: {
+    color: '#23262f',
+  },
   textColorWhite: {
     color: '#fff',
   },
